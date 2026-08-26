bde1f312fe4c4c2153ba07b07acd7bf3c604b53a
lumina-debug <luminaria504@gmail.com>
2026-08-26 12:51:54 +0900

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,5 +4,5 @@
  * 利用者は⚙から設定しなくてもすぐ使えます（画面から上書きも可能）。
  */
 window.HIKITSUGI_CONFIG = {
-  gasUrl: '', // 例: 'https://script.google.com/macros/s/XXXXXXXX/exec'
+  gasUrl: 'https://script.google.com/macros/s/AKfycbxP5ScPc1bsZXX37FwhYYk5Ek9FAH-NoNzd87VJQ-UuLUHHtqXv0AKnTmFA5jKPFMsq/exec', // 例: 'https://script.google.com/macros/s/XXXXXXXX/exec'
 };
